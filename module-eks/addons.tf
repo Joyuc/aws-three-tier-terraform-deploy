a2e0ff22eb2d6887611c62350493dbd9e7fc43cc
joyuc <cynthia4ukibe@gmail.com>
2025-06-21 19:30:06 +0100

    seventh commit

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,16 +1,16 @@
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
 provider "helm" {
     # kubernetes {
     #     host                   = aws_eks_cluster.eks.endpoint
     #     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
     #     token                  = data.aws_eks_cluster_auth.eks.token
     # }
-}
-
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 data "aws_eks_cluster_auth" "eks" {
@@ -25,6 +25,7 @@ resource "helm_release" "nginx_ingress" {
     create_namespace = true
 
     values = [file("${path.module}/nginx-ingress-values.yaml")]
+    provider         = kubernetes.eks
     depends_on = [ aws_eks_node_group.eks_node_group ]
 }
 
@@ -47,6 +48,7 @@ resource "helm_release" "cert_manager" {
         name  = "installCRDs"
         value = "true"
     }
+    provider         = kubernetes.eks
     depends_on = [ helm_release.nginx_ingress ]
 }
 #==================================================
@@ -59,5 +61,6 @@ resource "helm_release" "argocd" {
     namespace        = "argocd"
     create_namespace = true
     values = [file("${path.module}/argocd-values.yaml")]
+    provider         = kubernetes.eks
     depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager]
 }
