df48695228bb898a352e96ca845bef4f75b8600e
joyuc <cynthia4ukibe@gmail.com>
2025-06-21 19:57:02 +0100

    12

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -7,11 +7,11 @@ provider "kubernetes" {
 
 provider "helm" {
     alias = "eks"
-  kubernetes {
-    host                   = aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-    }
+#   kubernetes {
+#     host                   = aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#     }
 }
 
 data "aws_eks_cluster_auth" "eks" {
